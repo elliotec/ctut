05f012b6db99ffd07d3b0ef8835a8f294d8b9d0b
elliotec <elliotecweb@gmail.com>
2018-03-25 15:51:39 -0600

    hello world

---
+++ hello.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  hello world
+ *
+ *        Version:  1.0
+ *        Created:  03/25/2018 03:47:16 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Michael Elliott (),
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main() {
+  printf("Hello, world! \n");
+
+  return 0;
+}
+
